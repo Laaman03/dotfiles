2734e7287ed61f21300cf1c6e3f40f24f92ee8d1
Laaman03 <wfarrs@gmail.com>
2022-04-09 10:03:34 -0500

    fixed so .config files get deleted when necessary

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # copy all files to .config regardless of
 # wheter they will do anything there
 
+for d in xdg_config/*/; do
+	rm -rf $HOME/${d/xdg_/.}
+done
 cp -R xdg_config/* $HOME/.config
 
 # put the bash files where they belong
